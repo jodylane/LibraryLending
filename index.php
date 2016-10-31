fab0c94fff85c602ef5e4746b53136ec72f62324
Josh Lane <jodylane@umail.iu.edu>
2016-10-31 14:06:28 -0400

    add .idea/ to .gitignore

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Josh Lane
+ * Date: 10/31/2016
+ * Time: 2:02 PM
+ * Description: This file was created to
+ */ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+</body>
+</html>
