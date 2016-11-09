cf0df036d4092c0b5a238ae3047090cf47e58990
nekema <nekema@users.noreply.github.com>
2016-11-08 21:37:21 -0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: Nathan Ekema
+ * Date: 11/5/2016
+ * Description: This file was created to be used as the header section
+ */
+?>
+<header>
+
+  <nav>
+    <span><a href="">Sign In/Sign Up</a></span>
+    <a href="">Cart</a>
+    <a href="booklist.php">Book List</a>
+    <a href="about.php">About</a>
+    <a href="index.php">Home</a>
+  </nav>
+
+  <h2 class="headline">Best source for knowledge<h2>
+
+  <form>
+    <input class="search" type="text" name="search" placeholder=" Search...">
+  </form>
+
+  <p class="sub-headline">there are a lot of books here, search for them because there's a lot</p>
+
+</header>
