c81f9464a374c239cac1d623e38fa46fe9831eae
Nathan Ekema <nathanekema@140-182-236-209.dhcp-in.iupui.edu>
2016-11-09 10:34:38 -0500

    catching up

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Josh Lane
- * Date: 10/31/2016
- * Time: 2:02 PM
- * Description: This file was created to
- */ ?>
+ * User: Nathan Ekema
+ * Date: 11/5/2016
+ * Description: This file was created to display the home page
+ */
+?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-    <link href="css/style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
+  <html lang="en">
 
-</body>
-</html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Library Lending</title>
+      <link href="css/sections.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+
+      <?php include "includes/header.php"; ?>
+
+      <?php include "includes/featured.php"; ?>
+
+      <?php include "includes/footer.php"; ?>
+
+    </body>
+  </html>
