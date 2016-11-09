af849d099ef31a05bdd1bbc8384ea242a4433b73
Josh Lane <jodylane@umail.iu.edu>
2016-11-08 21:38:29 -0500

    navigation bar

---
+++ includes/navigation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Josh Lane
+ * Date: 11/8/2016
+ * Time: 7:36 PM
+ * Description: This file was created to
+ */ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<nav>
+    <div class="navbar">
+        <img src="../images/logo.png" class="logo"/>
+        <a href="#">Home</a>
+        <a href="#">List Books</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </div>
+</nav>
+
+</body>
+</html>
