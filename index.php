468d06e628f33c406b8d93722b19d9312bf0895a
Josh Lane <jodylane@umail.iu.edu>
2016-11-08 19:28:04 -0500

    added button and search bar styles

--- index.php
+++ index.php
@@ -14,6 +14,18 @@
     <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+<p>Hello</p>
+<button class="btn btn-default">Default</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-success">Success</button>
+<button class="btn btn-info">Info</button>
+<button class="btn btn-warning">Warning</button>
+<button class="btn btn-danger">Danger</button>
+<div>
+    <input class="search-input" placeholder="Search for..."/>
+    <span class="btn-search">Go!</span>
+</div>
 
+<input class="input" placeholder="Search for..."/>
 </body>
 </html>
