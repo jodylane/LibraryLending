1d701453d88cbc4ba6dee6806ab9301cfc638808
Nathan Ekema <nathanekema@140-182-237-83.dhcp-in.iupui.edu>
2016-11-21 10:43:36 -0500

    clean up

--- includes/header.php
+++ includes/header.php
@@ -7,30 +7,30 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Library Lending</title>
-  <link href="css/sections.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<header>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Library Lending</title>
+    <link href="css/sections.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <header>
 
-  <nav>
-    <span><a href="#">Sign In/Sign Up</a></span>
-    <a href="#">Cart</a>
-    <a href="booklist.php">Book List</a>
-    <a href="about.php">About</a>
-    <a href="index.php">Home</a>
-  </nav>
+    <nav>
+      <span><a href="#">Sign In/Sign Up</a></span>
+      <a href="#">Cart</a>
+      <a href="booklist.php">Book List</a>
+      <a href="about.php">About</a>
+      <a href="index.php">Home</a>
+    </nav>
 
-  <h2 class="headline">Best source for knowledge<h2>
+    <h2 class="headline">Best source for knowledge<h2>
 
-  <form>
-    <input class="search-input" type="text" name="search" placeholder=" Search for...">
-    <span class="btn-search">Go!</span>
-  </form>
+    <form>
+      <input class="search-input" type="text" name="search" placeholder=" Search for...">
+      <span class="btn-search">Go!</span>
+    </form>
 
-  <p class="sub-headline">there are a lot of books here, search for them because there's a lot</p>
+    <p class="sub-headline">there are a lot of books here, search for them because there's a lot</p>
 
-</header>
+  </header>
