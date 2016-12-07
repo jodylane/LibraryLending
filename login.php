1a4117c964759165fdbbe075c585bd76dba4ca18
Josh Lane <jodylane@umail.iu.edu>
2016-12-06 22:00:25 -0500

    styled login signup

--- login.php
+++ login.php
@@ -4,34 +4,16 @@
 require 'includes/database.php';
 require 'includes/header.php';
 ?>
-
+<section class="login">
     <form action="sendlogin.php" method="post">
-        <table class="bookdetails" >
-            <tr>
-                <td>
-                    <h4>Username:</h4>
-                    <h4>Password:</h4>
-                </td>
-                <td>
-                    <p><input required type="text" name="username"></p>
-                    <p><input required type="password" name="password"></p>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>
-                    <a href="index.php"><button type="button" href="index.php" class="btn btn-danger" formnovalidate>Cancel</button></a>
-                </td>
-                <td>
-                    <a href="signup.php"><button type="button" href="index.php" class="btn btn-info" formnovalidate>Register</button></a>
-                </td>
-                <td>
-                    <input type="submit" class="btn btn-success right" value="Log In">
-                </td>
-            </tr>
-        </table>
+        <input required type="text" name="username" class="input" placeholder="Username" autofocus>
+        <input required type="password" name="password" class="input" placeholder="Password">
+        <a href="index.php"><button type="button" href="index.php" class="btn btn-danger" formnovalidate>Cancel</button></a>
+        <input type="submit" class="btn btn-success" value="Log In">
+        <p>Don't have an account with us? Sign up now!</p>
+        <a href="signup.php"><button type="button" href="index.php" class="btn btn-info" formnovalidate>Signup</button></a>
     </form>
-
+</section>
 <?php
 require 'includes/footer.php'
 ?>
--- signup.php
+++ signup.php
@@ -1,44 +1,30 @@
 <?php
 /**
-<<<<<<< Updated upstream
+ * <<<<<<< Updated upstream
  * User: Nathan Ekema
  * Date: 11/5/2016
  * Description: This file was created to be used as the signup page
-
  */
 
 include "includes/header.php";
 include "includes/database.php";
 ?>
+<section class="signup">
+    <form action="sendsignup.php" method="post">
 
-<form action="sendsignup.php" method="post">
-    <table class="bookdetails" >
-        <tr>
-            <td>
-                <h4>Choose a username:</h4>
-                <h4>First Name:</h4>
-                <h4>Last Name:</h4>
-                <h4>E-mail:</h4>
-                <h4>Password:</h4>
-            </td>
-            <td>
-                <p><input required type="text" name="username"></p>
-                <p><input required type="text" name="firstname"></p>
-                <p><input required type="text" name="lastname"></p>
-                <p><input required type="email" name="email"></p>
-                <p><input required type="password" name="password"></p>
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <a href="index.php"><button type="button" href="index.php" class="btn btn-danger" formnovalidate>Cancel</button></a>
-            </td>
-            <td>
-                <input type="submit" class="btn btn-success right" value="Register">
-            </td>
-        </tr>
-    </table>
-</form>
+        <input required type="text" name="username" placeholder="Choose a username" class="input" autofocus>
+        <input required type="text" name="firstname" placeholder="First name" class="input">
+        <input required type="text" name="lastname" placeholder="Last name" class="input">
+        <input required type="email" name="email" placeholder="Email" class="input">
+        <input required type="password" name="password" placeholder="Password" class="input">
+
+        <a href="index.php">
+            <button type="button" href="index.php" class="btn btn-danger" formnovalidate>Cancel</button>
+        </a>
+
+        <input type="submit" class="btn btn-success" value="Sign Up">
+
+    </form>
+</section>
 
 <?php include "includes/footer.php"; ?>
